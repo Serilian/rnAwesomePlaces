eeee463ab0279ab04b82e7e1e0bc157aa6a918ae
fhagno@yahoo.com <fhagno@yahoo.com>
2019-04-09 16:23:52 +0200

    Added storing image on firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,13 +1,36 @@
-import { ADD_PLACE, DELETE_PLACE} from "./ActionTypes";
+import { ADD_PLACE, DELETE_PLACE } from "./ActionTypes";
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
-  };
-};
+    return dispatch => {
+      fetch(" https://us-central1-rnplaces-ee771.cloudfunctions.net/storeImage", {
+        method: "POST",
+        body: JSON.stringify({ image: image.base64 })
+
+      }).then(resp => resp.json())
+        .then(resp => {
+            const placeData = {
+              name: placeName,
+              location: location,
+              image: resp.imageUrl
+            };
+            console.log(placeData);
+            return fetch("https://rnplaces-ee771.firebaseio.com/places.json", {
+              method: "post",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify(placeData)
+
+            }).catch(err => console.log(err))
+              .then(resp => resp.json())
+              .then(resp => {
+                console.log(resp);
+              });
+          }
+        ).catch(err => console.log(err));
+    };
+  }
+;
 
 
 export const deletePlace = (key) => {
